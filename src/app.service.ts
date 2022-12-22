2e673780980c01a53e2c2b037f4bed25e5c4dcb6
degrammer <ruben@fusebit.io>
2022-12-22 15:57:13 -0500

    Add example for handling refresh_token

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,24 @@
-import {  Injectable } from '@nestjs/common';
+import {  HttpException, HttpStatus, Injectable } from '@nestjs/common';
 const jsforce = require('jsforce');
 
 @Injectable()
 export class AppService {
-  async loadContacts(options): Promise<string> {
-    const client = new jsforce.Connection(options);
-    const contacts = await client.query('SELECT count() FROM Contact');
+  async loadContacts(options, onRefresh): Promise<string> {
+  try {
+    const connection = new jsforce.Connection(options);
+    connection.on('refresh', function(accessToken, data){
+      onRefresh(data);
+    });
+    const contacts = await connection.query('SELECT count() FROM Contact');
     return `Success! Loaded ${contacts.totalSize} contacts from SFDC`;
+  } catch (error) {
+    throw new HttpException({
+      status: HttpStatus.BAD_REQUEST,
+      error: `Failed to load contacts from Salesforce, reason: ${error.message}`,
+    }, HttpStatus.FORBIDDEN, {
+      cause: error
+    });
+  }
   }
   
 }
